e0d7e6f3a9ac45d3134067e00267824614b42400
George Colletti Barajas <miracle@dreamsmith.net>
2023-02-27 11:48:11 -0600

    remove erroneous lines in lsp config

--- plugin/lsp.lua
+++ plugin/lsp.lua
@@ -22,16 +22,6 @@ lsp.configure("emmet_ls", {
 })
 
 local cmp = require("cmp")
-local types = require("cmp.types")
-
-local function deprioritize_snippet(entry1, entry2)
-	if entry1:get_kind() == types.lsp.CompletionItemKind.Snippet then
-		return false
-	end
-	if entry2:get_kind() == types.lsp.CompletionItemKind.Snippet then
-		return true
-	end
-end
 
 local cmp_select = { behavior = cmp.SelectBehavior.Select }
 local cmp_mappings = lsp.defaults.cmp_mappings({
@@ -47,23 +37,6 @@ cmp_mappings["<C-e>"] = nil
 
 lsp.setup_nvim_cmp({
 	mapping = cmp_mappings,
-	sorting = {
-		priority_weight = 2,
-		comparators = {
-			deprioritize_snippet,
-			-- the rest of the comparators are pretty much the defaults
-			cmp.config.compare.offset,
-			cmp.config.compare.exact,
-			cmp.config.compare.scopes,
-			cmp.config.compare.score,
-			cmp.config.compare.recently_used,
-			cmp.config.compare.locality,
-			cmp.config.compare.kind,
-			cmp.config.compare.sort_text,
-			cmp.config.compare.length,
-			cmp.config.compare.order,
-		},
-	},
 })
 
 lsp.set_preferences({

d4d4e8674d57af59fd79e98e4646ea9614720913
George Colletti Barajas <miracle@dreamsmith.net>
2023-03-01 09:37:59 -0600

    cleanup alpha configurations and fix functions/commands

--- lua/dream/alpha.lua
+++ lua/dream/alpha.lua
@@ -3,32 +3,6 @@ local dashboard = require("alpha.themes.dashboard")
 
 -- https://textfancy.com/text-art/
 dashboard.section.header.val = {
-	-- "                         ████                                        ████                        ",
-	-- "                       ██▓▓▓▓██                                    ██▒▒▓▓██                      ",
-	-- "                     ██▓▓██▓▓▓▓██        ████████████████        ██▓▓▓▓██▓▓██                    ",
-	-- "                     ████████▓▓▒▒██    ██▓▓██▓▓▓▓▓▓▓▓██▓▓██    ██▓▓▓▓████████                    ",
-	-- "                   ████▓▓▓▓▓▓████▒▒██▒▒▒▒██  ██▒▒▒▒██  ██▒▒████▒▒████▓▓▓▓▓▓████                  ",
-	-- "                 ████▒▒▓▓██████████▓▓▓▓▒▒██▓▓██▒▒▒▒██▓▓██▒▒██▓▓██████████▓▓▒▒████                ",
-	-- "               ██▓▓▒▒▒▒██▒▒▓▓▓▓▓▓▓▓██▓▓██▒▒██▒▒▒▒▒▒▒▒██▒▒██▓▓▓▓▒▒▒▒▓▓▓▓▒▒██▒▒▒▒▒▒██              ",
-	-- "           ████▓▓██████▓▓▓▓▓▓████▒▒██▓▓██▓▓▒▒▒▒▒▒▒▒▒▒▒▒▓▓██▒▒██▒▒████▓▓▓▓▓▓██████▓▓████          ",
-	-- "         ██▓▓▓▓██  ██▒▒▒▒████▓▓▒▒██▓▓████▓▓▓▓▒▒▒▒▒▒▒▒▓▓▓▓████▒▒██▓▓▓▓████▒▒▒▒██  ██▓▓▓▓██        ",
-	-- "     ████▓▓████    ██▒▒██  ██▓▓▓▓████▓▓████▓▓▓▓▓▓▓▓▓▓▓▓████▓▓████▓▓▓▓██  ██▒▒██    ████▓▓████    ",
-	-- "   ██▓▓▓▓██      ██▒▒██    ██▓▓▓▓██▓▓██▓▓▓▓██▓▓▓▓▓▓▓▓██▓▓▓▓██▓▓██▓▓▓▓██    ██▓▓██      ██▓▓▓▓██  ",
-	-- " ██▓▓████      ██▓▓██        ██▓▓████▓▓██▒▒▒▒██▓▓▓▓██▒▒▒▒██▓▓████▓▓██        ██▓▓██      ████▓▓██",
-	-- " ████        ██▓▓██          ██▒▒██  ██▓▓██▒▒██▓▓▓▓██▒▒██▓▓██  ██▒▒██          ██▓▓██        ████",
-	-- "           ██▓▓██            ██▒▒▒▒██  ████▒▒████▓▓██▒▒████  ██▒▒▒▒██            ██▓▓██          ",
-	-- "           ██▓▓██              ██▓▓██    ██▓▓██    ██▓▓██    ██▓▓██              ██▓▓██          ",
-	-- "         ██▓▓██                ██▓▓██      ██        ██      ██▓▓██                ██▓▓██        ",
-	-- "       ██▓▓██                  ██▓▓██                        ██▓▓██                  ██▓▓██      ",
-	-- "     ██▓▓██                    ██▓▓██                        ██▓▓██                    ██▓▓██    ",
-	-- "     ████                        ██▒▒██                    ██▓▓██                        ▓▓██    ",
-	-- "                                 ██▓▓██                    ██▓▓▓▓                                ",
-	-- "                                 ██▒▒▓▓                    ██▒▒██                                ",
-	-- "                                 ██▒▒██                    ██▓▓██                                ",
-	-- "                                   ██▒▒██                ██▓▓██                                  ",
-	-- "                                   ██▓▓██                ██▓▓██                                  ",
-	-- "                                   ██▓▓██                ██▓▓██                                  ",
-	-- "                                   ████                    ████                                  ",
 	"                                                                                                 ",
 	"             █████ ██        █████ ███      █████ ██                        █████   ██    ██     ",
 	"        ██████  ███     ██████  █ ██   ██████  ████ █    █████         ██████  █████ █████       ",
@@ -52,14 +26,18 @@ dashboard.section.header.val = {
 }
 
 dashboard.section.buttons.val = {
-	dashboard.button("e", "  > New file", ":ene <BAR> startinsert <CR>"),
-	dashboard.button("SPC p f", "  Find file", ":Telescope find_files hidden=true no_ignore=true<CR>"),
-	dashboard.button("SPC p h", "  Recently opened files", "<cmd>Telescope oldfiles<CR>"),
-	dashboard.button("SPC p r", "  Frecency/MRU"),
-	dashboard.button("SPC p z", "  Find word", "<cmd>Telescope live_grep<cr>"),
+	dashboard.button("e", "  New file", ":ene <BAR> startinsert <CR>"),
+	dashboard.button("SPC p f", "  Find file", ":lua require('telescope.builtin').find_files()<CR>"),
+	dashboard.button("SPC p h", "  Recently opened files", ":lua require('telescope.builtin').oldfiles()<CR>"),
+	dashboard.button("SPC p r", "  Frecency/MRU", ":lua require('telescope').extensions.frecency.frecency()"),
+	dashboard.button(
+		"SPC p z",
+		"  Find word",
+		":lua require('telescope.builtin').live_grep({ file_ignore_patterns = { 'package-lock.json', 'package.json', 'node_modules', '.git' } })<cr>"
+	),
 	-- dashboard.button("SPC f m", "  Jump to bookmarks"),
 	-- dashboard.button("SPC s l", "  Open last session", "<cmd>SessionManager load_last_session<CR>"),
-	dashboard.button("q", "  > Quit NVIM", ":qa<CR>"),
+	dashboard.button("q", "  Quit NVIM", ":qa<CR>"),
 }
 
 -- Send config to alpha

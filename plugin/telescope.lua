f8f96bd9497f4636b30a36e68b62ce64c04dccca
George Colletti Barajas <miracle@dreamsmith.net>
2023-03-01 08:53:30 -0600

    add alpha.lua configuration with telescope-frecency

--- plugin/telescope.lua
+++ plugin/telescope.lua
@@ -1,10 +1,10 @@
+local telescope = require("telescope")
 local builtin = require("telescope.builtin")
+
 vim.keymap.set("n", "<leader>pf", builtin.find_files, {})
 vim.keymap.set("n", "<leader>pg", builtin.git_files, {})
--- vim.keymap.set('n', '<leader>pz', function()
---   builtin.grep_string({search = vim.fn.input("Grep > ")})
--- end)
---[[ vim.keymap.set('n', '<leader>pz', builtin.live_grep, {}) ]]
+vim.keymap.set("n", "<leader>ph", builtin.search_history, {})
 vim.keymap.set("n", "<leader>pz", function()
 	builtin.live_grep({ file_ignore_patterns = { "package-lock.json", "package.json", "node_modules", ".git" } })
 end, {})
+vim.keymap.set("n", "<leader>pr", telescope.extensions.frecency.frecency, {})

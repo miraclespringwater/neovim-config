232c0717bc7acef7c096cd73bd45b2ec451c369d
George Colletti Barajas <miracle@dreamsmith.net>
2023-03-02 06:07:37 -0600

    add context highlighting

--- lua/dream/indent-blankline.lua
+++ lua/dream/indent-blankline.lua
@@ -2,9 +2,12 @@ vim.opt.list = true
 vim.opt.listchars:append("space:⋅")
 vim.opt.listchars:append("eol:↴")
 
+vim.cmd([[highlight IndentBlanklineContextStart guisp=#b8bb26 gui=underline]])
+vim.cmd([[highlight IndentBlanklineContextChar guifg=#b8bb26 gui=nocombine]])
+
 require("indent_blankline").setup({
-  space_char_blankline = " ",
-  show_current_context = true,
-  show_current_context_start = true,
-  disable_with_nolist = true,
+	space_char_blankline = " ",
+	show_current_context = true,
+	show_current_context_start = true,
+	disable_with_nolist = true,
 })
